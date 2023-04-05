d4f700a87314e3133c11816920298e2e3b742322
pd_sperrow <sperrow13579@gmail.com>
2023-04-06 00:58:47 +0600

    Pushlish react code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ProfileWindow from './ProfileWindow';
+
+const App = () => {
+  return (
+    <ProfileWindow />
+  );
+};
+
+export default App;
